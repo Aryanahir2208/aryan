a74e1c893a8d6907fc2b79748762554eb7666045
Aryan Ahir <avahir3681@gmail.com>
2022-07-19 21:54:12 +0530

    xyz

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import footer from './componanat/footer';
 import content from './componanat/content.js';
 export default function App() {
   return (
-  <>
+  <> 
+    {/* jay sanghani */}
     <content />
     </>
     
